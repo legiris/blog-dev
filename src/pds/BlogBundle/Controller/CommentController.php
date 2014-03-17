d5057426fe9bbb1cdf87817ed48851d31913986e
legiris <xis@email.cz>
2014-03-17 17:19:32 +0100

    Add form

--- src/pds/BlogBundle/Controller/CommentController.php
+++ src/pds/BlogBundle/Controller/CommentController.php
@@ -3,9 +3,29 @@
 namespace pds\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use pds\BlogBundle\Entity\Comment;
 
 class CommentController extends Controller
 {
+    public function addFormAction()
+    {
+        $comment = new Comment(); //toto ma vazbu primo na entitu, zohledni to treba max char
+        
+        $form = $this->createFormBuilder($comment)
+            ->add('nickname')
+            ->add('text', 'textarea', [
+                'attr' => [ 'cols' => '45', 'rows' => '5' ]
+            ])
+            ->add('Vložit', 'submit')
+            ->getForm()->createView()
+        ;
+        
+        return $this->render('pdsBlogBundle:Comment:addForm.html.twig', [
+            'form' => $form
+        ]);
+    }
+    
+    
     public function fetchAllByArticleIdAction($articleId)
     {
         $comments = $this->getDoctrine()
